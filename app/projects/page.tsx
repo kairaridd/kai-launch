21fd368429d5b6c386e58694c92e9a43446751cc
Kaira Riddell <kairaridd@gmail.com>
2026-09-16 22:16:29 -0400

    fix: project nav

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,6 @@
 import goalImage from '../images/micah-tindell-0LvgvMjomnk-unsplash_1.jpg';
 
-export default function HomePage() {
+export default function ProjectsPage() {
 
   return (
     <div>
@@ -15,13 +15,13 @@ export default function HomePage() {
 
         {/* Overlay Text */}
         <h1 className="relative z-10 text-4xl font-extrabold tracking-wide drop-shadow-md">
-          Welcome
+          My Projects
         </h1>
       </div>
 
-      <h1 style={{textAlign: 'center'}} className="text-3xl text-[#ffbf00ff] font-bold">Hello, nice of you to drop by! </h1>
+      <h1 className="text-3xl text-[#ffbf00ff] font-bold">Hello, nice of you to drop by! </h1>
       <br></br>
-      <p style={{textAlign: 'center'}}>This website serves as a central hub detailing my Co-Op placements and experiences during my time at the University of Guelph. It is a record of my journey in the big wide universe of Computer Science. Feel free to have a look around. Use the lefthand hamburger menu bar to explore.
+      <p >This website serves as a central hub detailing my Co-Op placements and experiences during my time at the University of Guelph. It is a record of my journey in the big wide universe of Computer Science. Feel free to have a look around. Use the lefthand hamburger menu bar to explore.
 </p>
 <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
 

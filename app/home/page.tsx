8e8dea37ac01fcf984f4b44138e3b090994db60f
Kaira Riddell <kairaridd@gmail.com>
2026-09-16 21:01:42 -0400

    Home Page added

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,30 @@
+import goalImage from '../images/micah-tindell-0LvgvMjomnk-unsplash_1.jpg';
+
 export default function HomePage() {
+
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-2">Home Page</h1>
-      <p >Welcome to Kai Launch! Select an item from the menu to navigate.</p>
+    <div>
+
+     {/* Hero Section */}
+      <div
+        className="relative w-full h-64 rounded-xl overflow-hidden bg-cover bg-center flex items-center justify-center text-white shadow-lg"
+        style={{ backgroundImage: `url(${goalImage.src || goalImage})` }}
+      >
+        {/* Dark Overlay for contrast */}
+        <div className="absolute inset-0 bg-black/40" />
+
+        {/* Overlay Text */}
+        <h1 className="relative z-10 text-4xl font-extrabold tracking-wide drop-shadow-md">
+          Welcome
+        </h1>
+      </div>
+
+      <h1 style={{textAlign: 'center'}} className="text-3xl text-[#ffbf00ff] font-bold">Hello, nice of you to drop by! </h1>
+      <br></br>
+      <p style={{textAlign: 'center'}}>This website serves as a central hub detailing my Co-Op placements and experiences during my time at the University of Guelph. It is a record of my journey in the big wide universe of Computer Science. Feel free to have a look around. Use the lefthand hamburger menu bar to explore.
+</p>
+<br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+
     </div>
   );
 }
